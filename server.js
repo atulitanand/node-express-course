23a052e0d0dc0e3118196a6c2d99fca1302b250a
ATULIT ANAND <atulitraj.anand@gmail.com>
2021-05-15 02:34:12 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
+
+app.get("/users", (req, res) => {
+  res.json({
+    success: true,
+    message: "successfully got users. nice!",
+    users: mockUserData,
+  });
+});
+
 app.listen(8000, function () {
-  console.log("Server up and running at https://localhost:8000");
+  console.log("Server up and running at http://localhost:8000/");
 });
